5688c53ff5f78062948f43e542ac6c7432fdb95d
jdnc <madhuraparikh@gmail.com>
2021-02-07 19:47:32 -0500

    refactor ppm to use new Color class

--- main.rs
+++ main.rs
@@ -3,6 +3,16 @@ mod vec3;
 type Point = vec3::Vec3;
 type Color = vec3::Vec3;
 
+
+fn write_color(pixel: Color) {
+    const MULT: f64 = 255.999;
+    let i_red = (pixel.x * MULT) as u64;
+    let i_green = (pixel.y * MULT) as u64;
+    let i_blue = (pixel.z * MULT) as u64;
+
+    print!("{} {} {}\n", i_red, i_green, i_blue);
+}
+
 // generate ppm image
 fn gen_ppm(width: u64, height: u64) {
 
@@ -11,16 +21,11 @@ fn gen_ppm(width: u64, height: u64) {
     for j in (1..height + 1).rev() {
         eprintln!("Scan lines remaining: {}", j);
         for i in 0..width {
-            let red = (i as f64) / ((width - 1) as f64);
-            let green = (j as f64) / ((height - 1) as f64);
-            let blue = 0.25;
-    
-            const MULT: f64 = 255.999;
-            let i_red = (red * MULT) as u64;
-            let i_green = (green * MULT) as u64;
-            let i_blue = (blue * MULT) as u64;
-
-            print!("{} {} {}\n", i_red, i_green, i_blue);
+            let pixel = Color{
+                x: (i as f64) / ((width - 1) as f64),
+                y: (j as f64) / ((height - 1) as f64),
+                z: 0.25,};
+            write_color(pixel);
         }
     }
 
